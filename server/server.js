f8b8a9bc2a7a23c92875de2d034a633fdd47c160
Vincent Xie <vxie@travelers.com>
2024-10-01 16:10:04 -0400

    server has been tested and works

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 // Imports
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors()); // enable cors
 const PORT = 3000;
 
 // GET Route for /api/planets
 app.get('/api/planets', async (req, res) => {
     try {
-        console.log(req);
+        res.send("Hello");
     } catch (e) {
         console.log("Error", e);
         res.status(500).send("There has been an error")
