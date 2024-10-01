8d92b3c61d1879ba3999018782fa4276e75f937d
Vincent Xie <vxie@travelers.com>
2024-10-01 16:02:17 -0400

    added server, currently does not work

---
+++ server/server.js
@@ -0,0 +1,22 @@
+// Imports
+import express from 'express';
+
+const app = express();
+app.use(cors()); // enable cors for all routes
+const PORT = 3000;
+
+// GET Route for /api/planets
+app.get('/api/planets', async (req, res) => {
+    try {
+        console.log(req);
+    } catch (e) {
+        console.log("Error", e);
+        res.status(500).send("There has been an error")
+    }
+});
+
+
+// Listening to port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
