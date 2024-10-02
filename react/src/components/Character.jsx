58cef75f7603f22a16293d9d4fb24b5082be1f51
Vincent Xie <vxie@travelers.com>
2024-10-02 11:36:02 -0400

    working character page

--- react/src/components/Character.jsx
+++ react/src/components/Character.jsx
@@ -3,34 +3,70 @@ import { useState, useEffect } from 'react';
 
 const Character = () => {
     const { id } = useParams();
-    const [character, setCharacter] = useState(null);
+    const [character, setCharacter] = useState({});
+    const [planet, setPlanet] = useState({});
+    const [films, setFilms] = useState([]);
+
+    const fetchCharacter = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/characters/${id}`);
+            const json_response = await response.json();
+            setCharacter(json_response);
+            fetchPlanet(json_response.homeworld);
+        } catch (err) {
+            console.error(err);
+        };
+    };
+
+    const fetchPlanet = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/planets/${id}`);
+            const json_response = await response.json();
+            setPlanet(json_response);
+        } catch (err) {
+            console.error(err);
+        };
+    };
+
+    const fetchFilms = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/characters/${id}/films`);
+            const json_response = await response.json();
+            setFilms(json_response);
+        } catch (err) {
+            console.error(err);
+        };
+    };
 
     useEffect(() => {
-        fetch(`http://localhost:3000/api/characters/${id}`)
-        .then(res => res.json())
-        .then(data => setCharacter(data))
-        .catch(err => console.error(err))
-    }, [id])
+        fetchCharacter();
+        fetchFilms();
+    }, [id]);
 
     return (
         <>
             <h1 id="name">{character?.name}</h1>
             <section id="generalInfo">
-                <p>Height: <span id="height"></span> cm</p>
-                <p>Mass: <span id="mass"></span> kg</p>
-                <p>Born: <span id="birth_year"></span></p>
-            </section>
-            <section id="species">
-                <h2>Species</h2>
-                <p><span id="species"></span></p>
+                <p>Height: {character?.height} cm</p>
+                <p>Mass: {character?.mass} kg</p>
+                <p>Born: {character?.birth_year}</p>
             </section>
             <section id="planets">
                 <h2>Homeworld</h2>
-                <p><span id="homeworld"></span></p>
+                <p>{planet?.name}</p>
             </section>
             <section id="films">
                 <h2>Films appeared in</h2>
-                <ul></ul>
+                <div>
+                    {films?.map(film => {
+                        return (
+                            <div>
+                                {film.title}
+                            </div>
+                        )
+                    })
+                    }
+                </div>
             </section>
         </>
     )
