2f5e33a5b1bbbf08c17a0100db2ef1b45e27623d
Vincent Xie <vxie@travelers.com>
2024-10-02 14:43:03 -0400

    partial search functionality

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,23 +1,32 @@
-import {useNavigate} from 'react-router-dom'
-import {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
     const [characters, setCharacters] = useState(null);
+    const [searchString, setSearchString] = useState("");
+    const [allCharacters, setAllCharacters] = useState(null);
     const navigate = useNavigate();
-    const handleClick = (id) =>{
+    const handleClick = (id) => {
         navigate(`/characters/${id}`)
     }
 
     useEffect(() => {
         fetch("http://localhost:3000/api/characters")
-          .then(res => res.json())
-          .then(data => setCharacters(data))
-          .catch(err => console.error(err));
-      }, [])
+            .then(res => res.json())
+            .then((data) => {
+                setCharacters(data);
+                setAllCharacters(data);
+            })
+            .catch(err => console.error(err));
+    }, [])
 
-      const filterCharacters = () => {
-        return;
-      }
+    const handleChange = (e) => {
+        setCharacters(allCharacters);
+        setSearchString(e.target.value);
+        const re = new RegExp(searchString, "i");
+        const matchingCharacters = characters.filter(character => re.test(character.name))
+        setCharacters(matchingCharacters);
+    };
 
     return (
         <>
@@ -25,7 +34,7 @@ const Home = () => {
                 <h1>Star Wars Universe Lookup</h1>
                 <label htmlFor="searchString">Who you looking for? <span className="small">(Regular expressions are cool
                     here)</span></label>
-                <input id="searchString" onInput={filterCharacters} autoComplete="off" />
+                <input id="searchString" onChange={handleChange} autoComplete="off" value={searchString}/>
             </div>
             <section id="charactersList">
                 {
