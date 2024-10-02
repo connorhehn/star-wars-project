375de7598d9e4e958a514598251bf6c73d15849e
Connor Hehn <connor.hehn@gmail.com>
2024-10-02 09:55:46 -0400

    add hooks to Home component

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -11,20 +11,11 @@ import {
 
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/characters")
-      .then(res => res.json())
-      .then(data => setCharacters(data))
-      .catch(err => console.error(err));
-  }, [])
-
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home data={characters}/>}/>
+          <Route exact path="/" element={<Home />}/>
           <Route exact path="/characters" element={<Character />}/>
 
         </Routes>
--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,23 +1,36 @@
 import {useNavigate} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
-const Home = (props) => {
-    const characters = props.data;
+const Home = () => {
+    const [characters, setCharacters] = useState(null);
     const navigate = useNavigate();
     const handleClick = (id) =>{
         navigate(`/characters?id=${id}`)
     }
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/characters")
+          .then(res => res.json())
+          .then(data => setCharacters(data))
+          .catch(err => console.error(err));
+      }, [])
+
+      const filterCharacters = () => {
+        return;
+      }
+
     return (
         <>
             <div>
                 <h1>Star Wars Universe Lookup</h1>
-                <label for="searchString">Who you looking for? <span className="small">(Regular expressions are cool
+                <label htmlFor="searchString">Who you looking for? <span className="small">(Regular expressions are cool
                     here)</span></label>
-                <input id="searchString" oninput="filterCharacters()" autocomplete="off" />
+                <input id="searchString" onInput={filterCharacters} autoComplete="off" />
             </div>
             <section id="charactersList">
                 {
-                    characters.map((character) => {
-                        return <div onClick={() => handleClick(character.id)}>{character.name}</div>
+                    characters?.map((character) => {
+                        return <div key={character.id} onClick={() => handleClick(character.id)}>{character.name}</div>
                     })
                 }
             </section>
